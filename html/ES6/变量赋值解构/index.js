82da4c9a29de9d06eda0c87e33c5a55e1dc6d921
pixinlei <2954716615@qq.com>
2020-07-21 11:06:07 +0800

    更新

---
+++ html/ES6/变量赋值解构/index.js
@@ -0,0 +1,24 @@
+// let [a,b,c] = [1,2,3]
+// console.log(c);
+
+// let [foo,[[bar],baz]] = [1,[[2],3]]
+// console.log(foo);
+// console.log(bar);
+// console.log(baz);
+
+// let [, , third] = ['a','b','c']
+// console.log(third);
+
+// let [x,y] = [1,2,3]
+// console.log(x);
+// console.log(y);
+
+// let [head,...tail] = [1,2,3,4]
+// console.log(head,tail);
+
+// let [foo] = 1
+// console.log(foo);  
+
+//对象的解构
+// let {foo,bar} = {foo:'aaa',bar:'bbbb'}
+// console.log(foo,bar);
