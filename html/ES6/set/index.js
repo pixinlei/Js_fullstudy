6bc66e718bba2799551f5a59babd1d29a7234b5b
pixinlei <2954716615@qq.com>
2020-07-24 12:07:50 +0800

    set

---
+++ html/ES6/set/index.js
@@ -0,0 +1,61 @@
+// const s = new Set() //类数组
+// const arr = new Array()
+// console.log(arr);
+// console.log(s);
+
+// let arr = [2,3,4,5,6,7,12,34,5]
+
+// for(let i=0;i<arr.length;i++){
+//   console.log(arr[i]);
+// }
+
+// for(let item of arr){
+//   console.log(item);
+// }
+
+// arr.forEach((x)=>{
+//   s.add(x)
+// })
+// console.log(s);
+
+// const set = new Set([1,2,3,4,4,4,5,5])
+// console.log(set.size);
+
+// const set = new Set(document.querySelectorAll('div'))
+// console.log(set.size);
+
+//数组去重
+// let arr = [1,2,3,4,5,6,1,2]
+// console.log([...new Set(arr)]);
+// Array.from(new Set(arr))
+
+//字符串去重
+// console.log([...new Set('abababababbababbabababa')]);
+
+// const set = new Set()
+// set.add(5)
+// set.add('5')
+// console.log(set);
+
+// const set = new Set()
+// set.add(1).add(2).add(4)
+// set.delete(2)
+// // console.log(set)
+// set.clear()
+// console.log(set);
+
+// let arr = [1,2,3]
+// console.log(arr.);
+
+//set遍历
+// const set = new Set(['red','green','blue'])
+// const set = new Set()
+// set.add({name:'wn'})
+// // keys()是set结构中返回键名的遍历器
+// // for(let item of set){
+// //   console.log(item);
+// // }
+
+// for(let item of set.values()){
+//   console.log(item);
+// }
