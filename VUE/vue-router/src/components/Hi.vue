799069ffcd0420512443a73a789572f141f525fb
pixinlei <2954716615@qq.com>
2020-08-25 12:04:35 +0800

    8-25

---
+++ VUE/vue-router/src/components/Hi.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+  <h1>{{msg}}</h1>
+  <router-link :to="{name:'hi1',params:{name:'乐怡'}}">hi 一</router-link>---------------
+  <router-link to="/hi/hi2">hi 二</router-link>
+
+  <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Hi',
+  data () {
+    return {
+      msg:'hi, i am wn'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/vue-router/src/components/Hi2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hi">我是hi二页面</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
