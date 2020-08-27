d3e9cd4d56afa7db9b9e1a7f1a159c820ce7492f
pixinlei <2954716615@qq.com>
2020-08-27 12:03:21 +0800

    8-27

---
+++ VUE/vue-component/src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user">
+    <p>{{userName}}</p>
+    <!-- <button @click="handle">按钮</button> -->
+    <button @click="changeBaby">养新的宠物</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['userName',],
+  data () {
+    return {
+      name:'皮皮',
+      title:'我是子组件的title',
+      chongwu:'小黑'
+    }
+  },
+  methods:{
+    // handle () {
+    //   // console.log(111111111);
+    //   this.$emit('titleChange',this.title)
+    // }
+    changeBaby () {
+      this.$emit('babyChange',this.chongwu)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/vue-component/src/components/animal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="animal">
+    <p>我的宠物：{{aaa}}</p>
+  </div>
+</template>
+
+<script>
+// import User from "@/components/User"
+export default {
+  props:{
+    aaa: {
+      type:String,
+      default: function () {
+        return '小白'
+      }
+    }
+  },
+  data () {
+    return {
+      baby:'小白'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
