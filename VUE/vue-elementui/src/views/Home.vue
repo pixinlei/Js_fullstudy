8f1178e33bb251c9282e4aadf018b90158cdc935
pixinlei <2954716615@qq.com>
2020-08-31 17:05:49 +0800

    8-31

---
+++ VUE/vue-elementui/src/views/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>
+        管理系统
+        <div class="user-control">
+          <el-dropdown @command="handleCommand">
+            <span class="el-dropdown-link">
+              {{ this.$route.params.user}}
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item command="setting">设置</el-dropdown-item>
+              <el-dropdown-item command="changepsw">修改密码</el-dropdown-item>
+              <el-dropdown-item command="loginOut">退出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            router
+            style="height:100%"
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="user">金龙</el-menu-item>
+                <el-menu-item index="user2">祥祥</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+
+
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    handleCommand(command) {
+      if (command === "loginOut") {
+        this.$router.push({ path: "/" });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.is-vertical {
+  height: 100vh;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
---
+++ VUE/vue-elementui/src/views/Login.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="login">
+    <h2>旅梦后台管理系统</h2>
+    <div class="login-wrapper">
+      <p class="input">
+        <el-input v-model="user" placeholder="请输入账号"></el-input>
+      </p>
+      <p class="input">
+        <el-input placeholder="请输入密码" v-model="passWord" show-password></el-input>
+      </p>
+      <p class="btn">
+        <el-button type="primary" round @click="login">登录</el-button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: "",
+      passWord: "",
+    };
+  },
+  methods: {
+    login() {
+      // if (this.user === "admin" && this.passWord === '123456') {
+      //   console.log("登陆成功");
+      // }else {
+      //   console.log('登陆失败');
+      // }
+
+      // if(!this.user || !this.passWord) {
+      //   this.$message.error('账号或密码不能为空');
+      // }else if (this.user === "admin" && this.passWord === '123456') {
+      //   this.$message.success('登陆成功');
+
+      //   //路由跳转
+      //   this.$router.push({ path:'/home'}) 
+      // }else {
+      //   this.$message.warning('账号或密码错误');
+      // }
+
+      if(!this.user || !this.passWord) {
+        this.$message.error('账号或密码不能为空');
+      }else {
+        this.$router.push({
+          name:'Home',
+          params : {
+            user: this.user,
+            passWord: this.passWord
+          }
+        }) 
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: aquamarine;
+}
+.login-wrapper {
+  margin-top: 30px;
+  width: 250px;
+}
+.input {
+  margin-bottom: 20px;
+}
+.btn button {
+  width: 100%;
+}
+</style>
