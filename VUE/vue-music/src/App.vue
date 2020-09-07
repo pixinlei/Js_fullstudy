7ede2ffc9c1814285400d07393358377f9212659
pixinlei <2954716615@qq.com>
2020-09-07 22:08:27 +0800

    9-7

---
+++ VUE/vue-music/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <!-- <router-view/> -->
+  </div>
+</template>
+<script>
+import Header from '@/components/header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>>
+<style lang="stylus">
+
+</style>
---
+++ VUE/vue-music/src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">123</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
