fc35c982c166a632a73a6b78c5c0ec7c4f6a9a63
pixinlei <2954716615@qq.com>
2020-09-15 22:11:58 +0800

    9-15

--- VUE/VUE-NODEBOOK/front/src/main.js
+++ VUE/VUE-NODEBOOK/front/src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
+import { Swipe, SwipeItem, Icon, Field, ActionSheet, Toast, Panel, Uploader } from 'vant';
+import 'vant/lib/index.css';
+import axios from 'axios'
+import util from './assets/js/util'
+
+Vue.use(Swipe).use(SwipeItem).use(Icon).use(Field).use(ActionSheet).use(Toast).use(Panel).use(Uploader).use(axios)
+
+Vue.prototype.$http = axios
+Vue.prototype.$util = util
+
+router.beforeEach((to,from,next) => {
+  document.title = to.meta.title
+  next()
+})
 
 Vue.config.productionTip = false
 
--- VUE/VUE-NODEBOOK/note-book/controllers/mySqlConfig.js
+++ VUE/VUE-NODEBOOK/note-book/controllers/mySqlConfig.js
@@ -1,21 +1,20 @@
 var mysql = require('mysql')
-const { database } = require('./defaultConfig')
 var config = require('./defaultConfig')
 
-// 连接线程池
+// 链接线程池
 var pool = mysql.createPool({
   host: config.database.HOST,
   user: config.database.USERNAME,
   password: config.database.PASSWORD,
   database: config.database.DATABASE,
-  port: config.database.PORT
+  port: config.database.PORT,
 })
 
-// 统一连接数据库的方法
+// 统一链接数据库的方法
 let allServices = {
-  query: function (sql, values) {
+  query: function(sql, values) {
     return new Promise((resolve, reject) => {
-      pool.getConnection(function (err, connection) { // 建立连接
+      pool.getConnection(function(err, connection) { // 建立链接
         if (err) {
           reject(err)
         } else {
@@ -25,7 +24,7 @@ let allServices = {
             } else {
               resolve(rows)
             }
-            connection.release() // 释放连接
+            connection.release() // 释放链接
           })
         }
       })
@@ -34,12 +33,34 @@ let allServices = {
 }
 
 // 读取所有users表数据
-let getAllUsers = function () {
-  let _sql = `select * from users;`
+let getAllUsers = function() {
+  let _sql =  `select * from users;`
   return allServices.query(_sql)
 }
 
+// 用户登录
+let userLogin = function(username, userpwd) {
+  let _sql = `select * from users where username="${username}" and userpwd="${userpwd}";`
+  return allServices.query(_sql)
+}
+
+// 查找用户
+let findUser = function(username) {
+  let _sql = `select * from users where username="${username}";`
+  return allServices.query(_sql)
+}
+
+// 用户注册
+let insertUser = function(value) {
+  let _sql = `insert into users set username=?,userpwd=?,nickname=?;`
+  return allServices.query(_sql, value)
+}
+
+
 // 导出方法
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  userLogin,
+  findUser,
+  insertUser
 }
