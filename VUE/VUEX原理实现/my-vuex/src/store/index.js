7c9f61abe76a33628fbc9b3716f55778ef16d35e
pixinlei <2954716615@qq.com>
2020-09-23 21:39:11 +0800

    9-23

---
+++ VUE/VUEX原理实现/my-vuex/src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from './myVuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 1
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
