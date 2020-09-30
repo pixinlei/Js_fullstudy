22de29418fce9a1cf9ccd516f21c4caed9c7816f
pixinlei <2954716615@qq.com>
2020-09-30 20:59:28 +0800

    9-30

--- VUE/VUEMUSIC/vue-music/src/views/musicDetail.vue
+++ VUE/VUEMUSIC/vue-music/src/views/musicDetail.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="page">
-    <div>{{list.name}}</div>
+    <div class="bgckground">
+    {{list.name}}
     <img :src="list.coverImgUrl" alt="">
-    <!-- <div v-for="(item,index) in list" :key="index">{{item}}</div> -->
+    </div>
+    <div class="item" v-for="item in list2" :key="item.id">
+      <div class="name">{{item.name}}</div>
+      <div class="desc"><span>{{item.ar[0].name}}</span><span>{{item.ar[1].name}}</span></div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +16,8 @@ import api from "@/api";
 export default {
   data() {
     return {
-      list: []
+      list: [],
+      list2: []
     }
   },
   methods: {
@@ -23,9 +29,10 @@ export default {
         }
         api.MusicDetail(params).then((res) => {
           that.list = res.playlist 
-          console.log(res.playlist);    
-          console.log(that.list.tracks[0].id); 
-          console.log(that.list.tracks);  
+          // console.log(res.playlist);    
+          // console.log(that.list.tracks[0].id); 
+          console.log(res.playlist.tracks);  
+          that.list2 = res.playlist.tracks
         })
       })
     }
@@ -37,7 +44,27 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+  .bgckground
+    width 100%
+    height 200px
+    position relative
+    img 
+      position absolute
+
+      width 100%
+
   img 
     width 100%
     height 30px
+  .item
+    width 100%
+    height 65px
+    padding 10px
+    box-sizing border-box
+    .name
+      margin 10px
+    .desc
+      margin-left 10px
+      margin-right 5px
+      color: rgba(255,255,255,0.5)
 </style>
---
+++ html/GET-POST/readme.md
@@ -0,0 +1,7 @@
+http 并没有规定 Get参数 一定放到url后面
+用post一定要放到请求体里面
+
+<!-- 语义区别 -->
+get -> post -> https
+
+axios
