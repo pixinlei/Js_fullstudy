27a226c19a97879fa3aaf4dd2a743ebd53e5c09b
pixinlei <2954716615@qq.com>
2020-10-20 12:05:26 +0800

    10-20

---
+++ Node/mykoa/index.js
@@ -0,0 +1,16 @@
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//   res.end('hello world')
+// })
+
+// server.listen(3000)
+
+// const Koa = require('koa')
+// const app = new Koa()
+
+// app.use((ctx, next) => {
+//   ctx.body = 'hello'
+// })
+
+// app.listen(3000)
