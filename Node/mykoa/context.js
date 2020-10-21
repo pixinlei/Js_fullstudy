9df2071c0dc448953557d5b7e533745bb0dc8307
pixinlei <2954716615@qq.com>
2020-10-21 22:37:20 +0800

    10-21

--- Node/mykoa/context.js
+++ Node/mykoa/context.js
@@ -1,3 +1,18 @@
 let proto = {}
 
+function defineGetter(prop, name) { //prop代表对象，name代表对象上的属性
+  proto.__defineGetter__(name, function() { //每个对象都有__defineGetter__方法
+    return this[prop][name]
+  }) 
+}
+function defineSetter(prop, name) {
+  proto.__defineGetter__(name, function() { //每个对象都有__defineGetter__方法
+    this[prop][name] = val
+  }) 
+}
+ 
+defineGetter('request','url')
+defineGetter('request','path')
+defineSetter('request','body')
+
 module.exports = proto
