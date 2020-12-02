9b240153df9d854efeb8122359b5f3d8e9b7a44c
pixinlei <2954716615@qq.com>
2020-12-02 18:11:00 +0800

    12-2

---
+++ 面试/蜗牛哥100道面试题/generator.js
@@ -0,0 +1,28 @@
+function* text() {
+  let a = 1 + 2
+  yield 2
+  yield 3
+}
+
+let b = text()
+console.log(b.next);
+
+// 原理
+function generator(cb) {
+  return (function() {
+    var object = {
+      next: 0,
+      stop: function() {}
+    }
+    return {
+      next: function() {
+        var ret = cb(object)
+        if(ret === undefined) return {value: undefined, done: true}
+        return {
+          value: ret,
+          done: false
+        }
+      }
+    }
+  })()
+}
