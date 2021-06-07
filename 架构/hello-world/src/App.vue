235a109955f79cf571540d3499f6ecc3ff7bb2bb
pixinlei <2954716615@qq.com>
2021-06-07 20:00:27 +0800

    11

---
+++ 架构/hello-world/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav">
+  </div>
+  <router-view />
+</template>
+
+<style lang="scss">
+body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, pre, form, fieldset, lengend, label, button, input, select, option, textarea, optgroup, table, thead, tbody, tfoot, tr, th, td, div, span, img, a, em, i, iframe, :before, :after{box-sizing: border-box; margin: 0; padding: 0;}
+body, button, input, select, option, textarea, optgroup, img{font: 12px/1 "Helvetica Neue", Helvetica, Arial, "PingFang SC", "Hiragino Sans GB", "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif; outline: none; border: none;}
+textarea{resize: none; overflow: auto;}
+ul, ol{list-style: none;}
+a{text-decoration: none; background-color: transparent; -webkit-text-decoration-skip: objects;}
+table{border-collapse: collapse; border-spacing: 0;}
+h1, h2, h3, h4, h5, h6{font-size: 100%;}
+article, aside, footer, header, nav, section, figcaption, figure, main, details, menu{display: block; box-sizing: border-box; margin: 0; padding: 0;}
+.clearfix:after{content:"";display:table;clear:both;}
+</style>
---
+++ 架构/hello-world/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    这里是home
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    
+  },
+});
+</script>
