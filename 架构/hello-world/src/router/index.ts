a13c3d816859b58df3fea358f7036ae2ee323dd0
pixinlei <2954716615@qq.com>
2021-06-08 16:53:57 +0800

    判断存在token，跳转登录页

--- 架构/hello-world/src/router/index.ts
+++ 架构/hello-world/src/router/index.ts
@@ -14,4 +14,11 @@ const router = createRouter({
   routes
 })
 
+// router.beforeEach((to, from, next) => {
+//   // 有token就可以正常跳转，没有就跳转到登录页面
+//   if(localStorage.getItem('token')) next()
+//   router.replace('login')
+//   next()
+// })
+
 export default router
