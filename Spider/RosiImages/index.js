d75e329618e9f8bb7d715f6297b2668526c7e96b
pixinlei <2954716615@qq.com>
2021-07-07 22:55:11 +0800

    1

--- Spider/RosiImages/index.js
+++ Spider/RosiImages/index.js
@@ -9,20 +9,19 @@ const { title } = require('process');
     const page = await browser.newPage();
     await page.setDefaultTimeout(0)
     let data = []
-    let num = 1
     for (let i = 0; i < 24; i++) {
-        await page.goto(`https://www.msgao.com/tag/baoru/${i==0 ? '' : `index_${i}.html`}`);
+        await page.goto(`https://www.msgao.com/tag/baoru/${i == 0 ? '' : `index_${i}.html`}`);
 
-        await page.waitForSelector('body > div > div > ul > li > a > img')
+        await page.waitForSelector('li img')
 
-        let title = await page.$$eval('body > div > div > ul > li > a > img',
-            (links) => links.map(x => x.title));
-        let cover = await page.$$eval('body > div > div > ul > li > a > img',
+        let title = await page.$$eval('li img',
+            (links) => links.map(x => x.alt));
+        let cover = await page.$$eval('li img',
             (links) => links.map(x => x.src));
-        let href = await page.$$eval('body > div:nth-child(13) > div > ul > li > a',
+        let href = await page.$$eval('body > div:nth-child(5) > div.taglist > ul > li > a.taglisthtitle',
             (links) => links.map(x => x.href));
 
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < title.length; i++) {
             let temp = {
                 title: title[i],
                 cover: cover[i],
@@ -31,16 +30,54 @@ const { title } = require('process');
             data.push(temp)
         }
         console.log('成功下载了一页');
-        if (i % 10 == 0) {
-            fs.writeFile(`./coverImgType/${num}.json`, JSON.stringify(data, null, "\t"), function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            num++
-        }
     }
+    await detail(data)
+})
 
+async function detail (data) {
+    let detailMap = new Map()
+    data.forEach((v, i) => {
+        let url = null
+        if (i == 0) {
+            url = v.href
+        }
+        else {
+            url = `${v.href}_i`
+        }
+        await page.goto(`${url}`);
+        await page.waitForSelector('#contbody > div > div > a > img')
+        // let cover = await page.$$eval('#contbody > div > div > a > img',
+        //     (links) => links.map(x => x.src));
+        let href = await page.$$eval('#contbody > div:nth-child(5) > div > div.page > a:nth-child(8)',
+            (links) => links.map(x => x.href));
+        let count = href[0].replace(/[^\d]/g, ' ')
+        let num = count.trim().split(' ')[1]
+        let data = []
+        for (let i = 0; i <= num.length; i++) {
+            let url = null
+            if (i == 0) {
+                url = v.href
+            }
+            else {
+                url = `${v.href}_i`
+            }
+            await page.goto(`${url}`);
+            await page.waitForSelector('#contbody > div > div > a > img')
+            let cover = await page.$$eval('#contbody > div > div > a > img',
+                (links) => links.map(x => x.src));
+            data.push(cover)
+        }
+        detailMap.set(url, data)
+    })
+}
 
-    await browser.close();
-})();
+// for(let i=0;i<data.length;i++) {
+//     await page.goto(`${url}`);
+// }
+fs.writeFile(`./ImageCover/.json`, JSON.stringify(data, null, "\t"), function (err) {
+    if (err) {
+        console.log(err);
+    }
+});
+    // await browser.close();
+}) ();
