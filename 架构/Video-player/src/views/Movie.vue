01e188130f863df377ae59c021d6a82026b8c742
皮新雷 <xl.pi@idiaoyan.com>
2021-08-16 09:22:44 +0800

    1

--- 架构/Video-player/src/views/Movie.vue
+++ 架构/Video-player/src/views/Movie.vue
@@ -1,9 +1,14 @@
 <template>
   <TitleBar :title="movieTitle" :showLeftButton="true" />
-  <iframe class="movie" :src="movieUrl" frameborder="0" width="100%" height="200"></iframe>
+  <div style="width:100%;height:200px;position:relative;">
+    <iframe class="movie" :src="movieUrl" frameborder="0" width="100%" height="200"></iframe>
+    <a :href="movieUrl" style="width:100%;height:100%;zIndex:10;position:absolute;left:0;top:0;">11111111</a>
+  </div>
   <van-row v-for="(item, index) in xunleiHref" :key="index">
     <van-col span="24">
-      <a :href="xunleiHref.magnetic">{{xunleiHref.magneticTitle}}</a>
+      <div>
+        <a :href="item.magnetic">{{item.magneticTitle}}</a>
+      </div>
     </van-col>
   </van-row>
   <Swiper :swipeList="swipeList" />
@@ -20,17 +25,18 @@ export default {
     Swiper
   },
   setup() {
+    setTimeout(() => {console.log(xunleiHref.value.magneticTitle);}, 5000)
     const route = reactive(useRoute())
     const queryData:any = route.query
 
     const swipeList = computed(() => {
       let list: any = queryData?.previewPicture
-      // return list.split(',')
-      return ['https://img2.baidu.com/it/u=1052437823,2294515059&fm=26&fmt=auto&gp=0.jpg']
+      return list.split(',')
+      // return ['https://img2.baidu.com/it/u=1052437823,2294515059&fm=26&fmt=auto&gp=0.jpg']
     })
     const xunleiHref = computed(() => {
       let magnetic: [] = queryData?.magnetic.split(',')
-      let magneticTitle :any = queryData.magnetic.split(',')
+      let magneticTitle :[] = queryData?.magneticTitle.split(',')
       let result:any = []
 
       magnetic.forEach((v,i) => {
@@ -38,7 +44,8 @@ export default {
           magnetic: magnetic[i],
           magneticTitle: magneticTitle[i]
         })
-      })
+      })      
+      
       return result
     })
     return {
@@ -55,7 +62,14 @@ export default {
   margin-top: 20px;
   margin-bottom: 20px;
 }
-img {
-  display: none !important;
+a {
+  // background: red;
+  color: black;
 }
+a:link {
+  color: black;
+}
+// img {
+//   display: none !important;
+// }
 </style>
