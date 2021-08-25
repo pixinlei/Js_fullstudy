26218d8932aec7d661634d1476b91599663d0aa7
皮新雷 <xl.pi@idiaoyan.com>
2021-08-25 16:50:23 +0800

    1

--- 架构/back-end/controllers/pictureSqlConfig.js
+++ 架构/back-end/controllers/pictureSqlConfig.js
@@ -44,17 +44,28 @@ let insertPictureCosplay= function (value) {
   let _sql = ``
     if(value.img) {
       _sql = `UPDATE picture_cosplay SET img="${value.img}" WHERE href="${value.href}";`
+      return allServices.query(_sql, value)
     } else {
-      _sql = `INSERT INTO picture_cosplay set href='${value.href}',title='${value.title}',cover='${value.cover}';`
+      console.log('添加之前');
+      getPictureCosplay(value.href).then(res => {
+        console.log(res, res.length, '查找到了吗');
+        if(res.length) return
+        console.log('开始添加了');
+        _sql = `INSERT INTO picture_cosplay set href='${value.href}',title='${value.title}',cover='${value.cover}';`
+        return allServices.query(_sql, value)
+      }) // 查找是否已经添加过
     }
-    return allServices.query(_sql, value)
   }
 // 获取cosplay模块内容数据
 let getPictureCosplay = function (value) {
     let _sql = `select * from picture_cosplay LIMIT ${value.start},${value.end};`
     return allServices.query(_sql)
   }
-
+// 查找是否已经添加过
+let getPictureCosplay = function (href) {
+  let _sql = `select * from picture_cosplay where href="${href}";`
+  return allServices.query(_sql)
+}
 // 导出方法
 module.exports = {
   insertPictureCosplay,
