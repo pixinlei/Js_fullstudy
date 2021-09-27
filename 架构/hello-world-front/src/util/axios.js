a6c36de244c5aad6998be414b26c4e1933023c7c
皮新雷 <xl.pi@idiaoyan.com>
2021-09-27 17:58:39 +0800

    1

--- 架构/hello-world-front/src/util/axios.js
+++ 架构/hello-world-front/src/util/axios.js
@@ -1,17 +1,18 @@
 /**axios封装
  * 请求拦截、相应拦截、错误统一处理
  */
- import axios from 'axios';
- import QS from 'qs';
- import { Toast } from 'vant';
- import store from '../store/index'
+import axios from 'axios';
+import QS from 'qs';
+import { Toast } from 'vant';
+import { ElMessage } from 'element-plus'
+import store from '../store/index'
 
- // 环境的切换
-if (process.env.NODE_ENV == 'development') {    
+// 环境的切换
+if (process.env.NODE_ENV == 'development') {
     axios.defaults.baseURL = 'http://localhost:3000/';
-} else if (process.env.NODE_ENV == 'debug') {    
+} else if (process.env.NODE_ENV == 'debug') {
     axios.defaults.baseURL = '';
-} else if (process.env.NODE_ENV == 'production') {    
+} else if (process.env.NODE_ENV == 'production') {
     axios.defaults.baseURL = 'http://pixinlei.ltd:3000';
 }
 
@@ -27,97 +28,98 @@ axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded
 let pending = [] //声明一个数组用于储存每个ajax请求的取消函数和ajax标识
 let cancelToken = axios.CancelToken
 function removePending(ever) {
-    for(let p in pending) {
-        if(pending[p].u == ever.url + '&' + ever.method) {
+    for (let p in pending) {
+        if (pending[p].u == ever.url + '&' + ever.method) {
             pending[p].f() //执行取消操作
-            pending.splice(p,1) //删除这条记录
+            pending.splice(p, 1) //删除这条记录
         }
     }
 }
 
 
 // 请求拦截器
-axios.interceptors.request.use(    
+axios.interceptors.request.use(
     config => {
         // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
         // 即使本地存在token，也有可能token是过期的，所以在响应拦截器中要对返回状态进行判断
-        const token = window.localStorage.getItem('token');        
-        token && (config.headers.Authorization = token);      
-        
+        const token = window.localStorage.getItem('token');
+        token && (config.headers.Authorization = token);
+
         removePending(config) //在一个ajax发送前执行一下取消操作
         config.cancelToken = new cancelToken((c) => {
-            pending.push({u: config.url + '&' + config.method, f:c})
+            pending.push({ u: config.url + '&' + config.method, f: c })
         })
-        return config;    
-    },    
-    error => {        
-        return Promise.error(error);    
+        return config;
+    },
+    error => {
+        return Promise.error(error);
     })
 
 // 响应拦截器
-axios.interceptors.response.use(    
-    response => {        
+axios.interceptors.response.use(
+    response => {
         removePending(response.config);  //在一个ajax响应后再执行一下取消操作，把已经完成的请求从pending中移除
-        if (response.status === 200) {            
-            return Promise.resolve(response);        
-        } else {            
-            return Promise.reject(response);        
-        }    
+        if (response.status === 200) {
+            return Promise.resolve(response);
+        } else {
+            return Promise.reject(response);
+        }
     },
     // 服务器状态码不是200的情况    
-    error => {        
-        if (error.response.status) {            
-            switch (error.response.status) {                
-                // 401: 未登录                
-                // 未登录则跳转登录页面，并携带当前页面的路径                
-                // 在登录成功后返回当前页面，这一步需要在登录页操作。                
-                case 401:                    
-                    // router.replace({                        
-                    //     path: '/login',                        
-                    //     query: { redirect: router.currentRoute.fullPath } 
-                    // });
-                    break;
-                // 403 token过期                
-                // 登录过期对用户进行提示                
-                // 清除本地token和清空vuex中token对象                
-                // 跳转登录页面                
-                case 403:                     
-                    Toast({                        
-                        message: '登录过期，请重新登录',                        
-                        duration: 1000,                        
-                        forbidClick: true                    
-                    });                    
-                    // 清除token                    
-                    localStorage.removeItem('token');                    
-                    // store.commit('loginSuccess', null);                    
-                    // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面
-                    // setTimeout(() => {                        
-                    //     router.replace({                            
-                    //         path: '/login',                            
-                    //         query: { 
-                    //             redirect: router.currentRoute.fullPath 
-                    //         }                        
-                    //     });                    
-                    // }, 1000);                    
-                    break; 
-                // 404请求不存在                
-                case 404:                    
-                    Toast({                        
-                        message: '网络请求不存在',                        
-                        duration: 1500,                        
-                        forbidClick: true                    
-                    });                    
-                break;                
-                // 其他错误，直接抛出错误提示                
-                default:                    
-                    Toast({                        
-                        message: error.response.data.message,                        
-                        duration: 1500,                        
-                        forbidClick: true                    
-                    });            
-            }            
-            return Promise.reject(error.response);        
-        }       
+    error => {
+        ElMessage.error('请求发生错误')
+        // if (error.response.status) {         
+        //     switch (error.response.status) {                
+        //         // 401: 未登录                
+        //         // 未登录则跳转登录页面，并携带当前页面的路径                
+        //         // 在登录成功后返回当前页面，这一步需要在登录页操作。                
+        //         case 401:                    
+        //             // router.replace({                        
+        //             //     path: '/login',                        
+        //             //     query: { redirect: router.currentRoute.fullPath } 
+        //             // });
+        //             break;
+        //         // 403 token过期                
+        //         // 登录过期对用户进行提示                
+        //         // 清除本地token和清空vuex中token对象                
+        //         // 跳转登录页面                
+        //         case 403:                     
+        //             Toast({                        
+        //                 message: '登录过期，请重新登录',                        
+        //                 duration: 1000,                        
+        //                 forbidClick: true                    
+        //             });                    
+        //             // 清除token                    
+        //             localStorage.removeItem('token');                    
+        //             // store.commit('loginSuccess', null);                    
+        //             // 跳转登录页面，并将要浏览的页面fullPath传过去，登录成功后跳转需要访问的页面
+        //             // setTimeout(() => {                        
+        //             //     router.replace({                            
+        //             //         path: '/login',                            
+        //             //         query: { 
+        //             //             redirect: router.currentRoute.fullPath 
+        //             //         }                        
+        //             //     });                    
+        //             // }, 1000);                    
+        //             break; 
+        //         // 404请求不存在                
+        //         case 404:                    
+        //             Toast({                        
+        //                 message: '网络请求不存在',                        
+        //                 duration: 1500,                        
+        //                 forbidClick: true                    
+        //             });                    
+        //         break;                
+        //         // 其他错误，直接抛出错误提示                
+        //         default:                    
+        //             Toast({                        
+        //                 message: error.response.data.message,                        
+        //                 duration: 1500,                        
+        //                 forbidClick: true                    
+        //             });            
+        //     }            
+        //     return Promise.reject(error.response);        
+        // }       
     }
 );
 
@@ -126,17 +128,17 @@ axios.interceptors.response.use(
  * @param {String} url [请求的url地址] 
  * @param {Object} params [请求时携带的参数] 
  */
- export function get(url, params = {}){    
-    return new Promise((resolve, reject) =>{        
-        axios.get(url, {            
-            params: params        
-        })        
-        .then(res => {            
-            resolve(res.data);        
-        })        
-        .catch(err => {            
-            reject(err.data)        
-        })    
+export function get(url, params = {}) {
+    return new Promise((resolve, reject) => {
+        axios.get(url, {
+            params: params
+        })
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err.data)
+            })
     });
 }
 /** 
@@ -144,14 +146,14 @@ axios.interceptors.response.use(
  * @param {String} url [请求的url地址] 
  * @param {Object} params [请求时携带的参数] 
  */
-export function post(url, params = {}) {    
-    return new Promise((resolve, reject) => {         
-        axios.post(url, QS.stringify(params))        
-        .then(res => {            
-            resolve(res.data);        
-        })        
-        .catch(err => {            
-            reject(err.data)        
-        })    
+export function post(url, params = {}) {
+    return new Promise((resolve, reject) => {
+        axios.post(url, QS.stringify(params))
+            .then(res => {
+                resolve(res.data);
+            })
+            .catch(err => {
+                reject(err.data)
+            })
     });
 }
